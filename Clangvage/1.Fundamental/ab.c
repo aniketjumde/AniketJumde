0e05df1e4d05a76cf48232b1eaa6fe6f6b8ff755
Aniket Jumde <158746574+aniketjumde@users.noreply.github.com>
2024-03-01 22:48:36 +0530

    Add files via upload

---
+++ Clangvage/1.Fundamental/ab.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    char name;
+    printf("Enter the name:");
+    scanf("%10c", &name);
+
+    if(name=='Aniket')
+    {
+        printf("Aniket jumde");
+    }
+
+    return 0;
+}
