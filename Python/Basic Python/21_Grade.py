d5c927aef17d7fa3e8cad92721c1ef14c5751aa0
Aniket Jumde <158746574+aniketjumde@users.noreply.github.com>
2024-08-09 14:48:18 +0530

    Add files via upload

---
+++ Python/Basic Python/21_Grade.py
@@ -0,0 +1,16 @@
+n=float(input("Enter the grade  of student :"))
+
+if 100<n and 91>n :
+  			 print("O grade")
+elif  n<91 and n>81 :
+  			 print(" A+ Grade ")
+elif  n<81 and n>71 :
+  			 print(" A Grade ")
+elif  n<71 and n>61 :
+  			 print(" B Grade ")
+elif  n<61 and n>51 :
+ 			 print(" C Grade ")   
+elif  n<51 and n>41 :
+			 print(" D Grade ")   
+else :
+ 			 print("fail"	)             
